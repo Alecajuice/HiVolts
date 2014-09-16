d739a2a6c0765512c78c2c55a44ab1d112099b40
James Ngo <ducngocngo2@gmail.com>
2014-09-16 13:13:25 -0700

    made JFrame in gui

--- Hivolts/src/graphics/Gui.java
+++ Hivolts/src/graphics/Gui.java
@@ -1,8 +1,17 @@
 package graphics;
 
+import javax.swing.JFrame;
+
 //Main Gui class
 
-public class Gui
+public class Gui extends JFrame 
 {
 
+	public Gui(int width, int height) 
+	{
+		super("Hivolts");
+		setSize(width, height);
+		setVisible(true);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
 }
