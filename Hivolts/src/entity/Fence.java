5cd4a43d87a579b59772f4d9d6c81bb30ae58976
Alecajuice <alecadad@gmail.com>
2014-09-16 13:43:37 -0700

    4mating

--- Hivolts/src/entity/Fence.java
+++ Hivolts/src/entity/Fence.java
@@ -5,7 +5,8 @@ import grid.Tile;
 public class Fence extends Tile
 {
 
-	public Fence(int x, int y) {
+	public Fence(int x, int y)
+	{
 		super(x, y);
 		// TODO Auto-generated constructor stub
 	}
--- Hivolts/src/graphics/GridPanel.java
+++ Hivolts/src/graphics/GridPanel.java
@@ -4,15 +4,17 @@ import java.awt.*;
 import javax.swing.*;
 
 //One game grid
-public class GridPanel extends JPanel {
+public class GridPanel extends JPanel
+{
 
 	private int height;
 	private int width;
 
-	public GridPanel(int width, int height) {
+	public GridPanel(int width, int height)
+	{
 		this.height = height;
 		this.width = width;
-		
+
 		this.setSize(width, height);
 	}
 }
