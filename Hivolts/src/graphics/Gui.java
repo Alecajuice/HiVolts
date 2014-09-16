da93f759974155e4b1d7fd8ae92893bf7b1f49f5
Alecajuice <alecadad@gmail.com>
2014-09-16 13:23:58 -0700

    format stop nhar

--- Hivolts/src/graphics/Gui.java
+++ Hivolts/src/graphics/Gui.java
@@ -4,8 +4,10 @@ import javax.swing.JFrame;
 
 //Main Gui class
 
-public class Gui extends JFrame {
-	public Gui(int width, int height) {
+public class Gui extends JFrame
+{
+	public Gui(int width, int height)
+	{
 		super("Hivolts");
 		setSize(width, height);
 		setVisible(true);
