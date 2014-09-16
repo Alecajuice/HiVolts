f64b846fc76fc3aa90259d569b52ea91eec383b3
James Ngo <ducngocngo2@gmail.com>
2014-09-16 13:39:11 -0700

    fixed errors

--- Hivolts/src/entity/Cell.java
+++ Hivolts/src/entity/Cell.java
@@ -1,5 +1,13 @@
 package entity;
 
-public class Cell {
+import grid.Tile;
+
+public class Cell extends Tile
+{
+
+	public Cell(int x, int y) {
+		super(x, y);
+		// TODO Auto-generated constructor stub
+	}
 
 }
--- Hivolts/src/entity/Player.java
+++ Hivolts/src/entity/Player.java
@@ -5,4 +5,9 @@ import grid.Tile;
 public class Player extends Tile
 {
 
+	public Player(int x, int y) {
+		super(x, y);
+		// TODO Auto-generated constructor stub
+	}
+
 }
