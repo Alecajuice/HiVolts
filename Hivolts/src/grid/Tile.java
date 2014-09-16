826a98a82920601f8bf4a8070be27157a7a27e0d
James Ngo <ducngocngo2@gmail.com>
2014-09-16 13:37:16 -0700

    added x and y for tile

--- Hivolts/src/grid/Tile.java
+++ Hivolts/src/grid/Tile.java
@@ -4,8 +4,10 @@ import java.awt.Graphics;
 
 public class Tile
 {
-	public Tile() {
-		
+	private int x, y;
+	public Tile(int x, int y) {
+		this.x = x;
+		this.y = y;
 	}
 	
 	public void draw(Graphics g) {
