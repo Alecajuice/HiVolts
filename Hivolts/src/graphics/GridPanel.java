c557cc2fa5f24de0453f0851a4915678c6f42b31
James Ngo <ducngocngo2@gmail.com>
2014-09-16 13:44:23 -0700

    fixed gridpanel

--- Hivolts/src/graphics/GridPanel.java
+++ Hivolts/src/graphics/GridPanel.java
@@ -1,6 +1,9 @@
 package graphics;
 
+import grid.Tile;
+
 import java.awt.*;
+
 import javax.swing.*;
 
 //One game grid
@@ -8,6 +11,7 @@ public class GridPanel extends JPanel {
 
 	private int height;
 	private int width;
+	private Tile[][] grid = new Tile[12][12];
 
 	public GridPanel(int width, int height) {
 		this.height = height;
@@ -15,4 +19,5 @@ public class GridPanel extends JPanel {
 		
 		this.setSize(width, height);
 	}
+	
 }
