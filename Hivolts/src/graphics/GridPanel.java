7c68bb3420940a26ed5b34abc07e6b510d0ceecb
Alecajuice <alecadad@gmail.com>
2014-09-22 14:11:47 -0700

    creating a merge error for james because i need to add a method

--- Hivolts/src/graphics/GridPanel.java
+++ Hivolts/src/graphics/GridPanel.java
@@ -1,25 +1,35 @@
 package graphics;
 
 import entity.Cell;
+import entity.mob.Player;
 
 import java.awt.*;
 
 import javax.swing.*;
 
 //One game grid
-public class GridPanel extends JPanel
-{
+public class GridPanel extends JPanel {
 
 	private int height;
 	private int width;
 	private Cell[][] grid = new Cell[12][12];
 
-	public GridPanel(int width, int height)
-	{
+	public GridPanel(int width, int height) {
 		this.height = height;
 		this.width = width;
 
 		this.setSize(width, height);
 	}
-	
+
+	public Player findPlayer() {
+		for (Cell[] c : grid) {
+			for (Cell cell : c) {
+				if (cell instanceof Player) {
+					return (Player) cell;
+				}
+			}
+		}
+		System.err.println("Player not found.");
+		return null;
+	}
 }
