540587c9f795447ebb95c1a477c11d38a4583a7f
95016499 <95016499@PH-Lab-CL2-16.palo-alto.edu>
2014-09-23 13:43:04 -0700

    xy not final anymore

--- Hivolts/src/entity/Cell.java
+++ Hivolts/src/entity/Cell.java
@@ -3,7 +3,7 @@ package entity;
 import java.awt.*;
 
 public class Cell {
-	protected final int x, y;
+	protected int x, y;
 	protected Color color = Color.black;
 
 	public Cell(int x, int y) {
@@ -20,11 +20,5 @@ public class Cell {
 	}
 	public int getY() {
 		return this.y;
-	}
-	public void setY(int y) {
-		this.y = y;
-	}
-	public void setX(int x) {
-		this.x = x;
 	}
 }
