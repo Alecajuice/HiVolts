5f9e74a83e9587b0571f243fe07d371e0bae7f39
Alecajuice <alecadad@gmail.com>
2014-09-30 13:45:11 -0700

    dfef

--- Hivolts/src/main/Main.java
+++ Hivolts/src/main/Main.java
@@ -5,6 +5,6 @@ import graphics.Gui;
 public class Main {
 	public static void main(String[] args) {
 		//Pass width and height of the game board
-		Gui gui = new Gui(15, 15);
+		Gui gui = new Gui(12, 12);
 	}
 }
