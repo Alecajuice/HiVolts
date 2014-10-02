fe233b973f97586c944c95857a2946ca2acd2c5b
95016499 <95016499@PH-Lab-CL2-16.palo-alto.edu>
2014-10-02 13:29:55 -0700

    random

--- Hivolts/src/main/Main.java
+++ Hivolts/src/main/Main.java
@@ -5,6 +5,6 @@ import graphics.Gui;
 public class Main {
 	public static void main(String[] args) {
 		//Pass width and height of the game board
-		Gui gui = new Gui(false, 12, 12);
+		Gui gui = new Gui(false, 15, 15);
 	}
 }
