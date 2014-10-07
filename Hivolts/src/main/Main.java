48db54808c93d0376efd99a44e0f4f5345768b2d
James Ngo <ducngocngo2@gmail.com>
2014-10-07 12:31:31 -0700

    added comments

--- Hivolts/src/main/Main.java
+++ Hivolts/src/main/Main.java
@@ -9,11 +9,21 @@ import entity.Cell;
 import entity.Fence;
 import graphics.GridPanel;
 import graphics.Gui;
+/*AUTHORS*/
+//Alex Tarng, Nihar Mitra, James Ngo, Ailyn Tong
 
+//This is the main class for Hivolts. The main creates a Gui where the player can move around and interact with elements in the game
+//The controls are q,w,e,a,s,d,z,x,c or the numpad. If you want to jump press j. 
+
+/*
+ * Goals:
+ * make a two player game
+ */
 public class Main {
 	public static void main(String[] args) {
 		//Pass width and height of the game board
 		//Also passes the existence of a second player
+		//initializes the pictures for the game
 		try
 		{
 		    Fence.img = ImageIO.read(new File("res/img/Kuszmaul.jpg"));
